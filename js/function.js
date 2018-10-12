729d3f35db7b99ac248bcffc87c5d783cdf0f3c3
user <user>
2018-10-12 15:14:40 +0530

    updates

--- js/function.js
+++ js/function.js
@@ -51,6 +51,37 @@ function getCategory(select_id, user_id)
    });
 }
 
+function loginNow(){
+	var customer_mobile = $('input[name="customer_mobile"]').val();
+	var action_url = siteurl + 'account/login';
+	
+	var device_platform = device.platform;
+	var device_reg_id = device.uuid;
+				
+	$.ajax({
+	 type: 'POST',
+	 url: action_url,
+	 dataType: 'json',
+	 data: {customer_mobile:customer_mob, latitude: lat, longitude:longi, device_type: device_platform, device_reg_id:device_reg_id},
+	 crossDomain: true,
+	 success: function(data){
+		
+	   	if(data['success'])
+		{
+			$('#mobile_no_container').remove();
+			$('#customer_mob').val(data['success']['no']);
+			$('#otp_container').show();
+		}
+		
+		if(data['error'])
+		{
+			$('#customer_mobile').addClass('req');
+		}
+	 }
+   });
+	//
+}
+
 function getState(select_val)
 {
 	var action_url = siteurl + 'common/common/getState';	
@@ -88,12 +119,34 @@ function getCity(select_val, state_id)
 }
 
 function checkLogin(){
+	
 	if(localStorage.getItem("cust_id") == null) window.location = 'phone-login.html';
 }
 
 function logOut(){
 	localStorage.removeItem('cust_id');
 	window.location = 'phone-login.html';
+}
+
+function chatMessage(current_user){
+	var action_url = siteurl + 'account/chat/message';	
+	$.ajax({
+	 type: 'POST',
+	 url: action_url,
+	 dataType: 'json',
+	 data: {login_id:current_user},
+	 crossDomain: true,
+	 success: function(data){
+	   	if(data['success'])
+		{
+			var show_chat_msg = '<div class="alertbox alert alert-danger alert-dismissible"><a href="your-connections.html">You have new chat message....</a>';
+			
+      show_chat_msg += '<button type="button" class="close" data-dismiss="alert">&times;</button></div>';
+	  
+			$('#bottom_bar').after(show_chat_msg);
+		}
+	 }
+   });
 }
 
 $(document).ready(function(e){
